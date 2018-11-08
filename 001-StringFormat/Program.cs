128d698a97ba4c86faccbb0bf00503aeb9456869
tshaiman <tomer.shaiman@gmail.com>
2018-11-08 14:07:10 +0200

    string format

--- 001-StringFormat/Program.cs
+++ 001-StringFormat/Program.cs
@@ -10,6 +10,49 @@ namespace _001_StringFormat
     {
         static void Main(string[] args)
         {
+            //primitive types :
+            int i = 3;
+            i++;
+
+            float f = 123.45f;
+            double d1 = 12.45999999;
+
+            string str = "hello";
+            string myStr;
+
+            myStr = "hello";
+            //Immutable String 
+            myStr = "world";
+            myStr.ToUpper();
+
+            Console.WriteLine("Welocome To My Course " + i);
+            Console.WriteLine("MyNum : {0} ,my Double: {1:f2} , myStr: {2}",i,d1,myStr);
+
+
+            /// Read from input
+            Console.WriteLine("Please Enter your name : " );
+            string name = Console.ReadLine();
+            Console.WriteLine("Welcome to C# {0}",name.ToUpper() );
+            string format = $"Welcome to C# {name.ToUpper()} and {4+6}";
+            Console.WriteLine($"Welcome to C# {name.ToUpper()} and {{0:f2}}",i*f);
+            Console.WriteLine(format);
+
+
+            //Date Time
+            DateTime dt = DateTime.Now;
+            double ms = (dt - new DateTime(1, 1, 1970)).TotalMilliseconds;
+
+            Console.WriteLine(dt);
+            Console.WriteLine($"day = {dt.Day} / {dt.Month} / {dt.Year} / {dt.Millisecond}");
+
+            Console.WriteLine("{0:o}",dt);
+
+
+            Console.WriteLine("Please enter your age");
+            String ageStr = Console.ReadLine();
+
+            int age = int.Parse(ageStr);
+
         }
     }
 }
