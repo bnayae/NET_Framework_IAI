f494018ec5b1a8238560bb78689705509807003f
tshaiman <tomer.shaiman@gmail.com>
2018-11-15 14:26:07 +0200

    class members

---
+++ 002-ClassMembers/Foo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace _004_Class_Members
+{
+    public class Foo
+    {
+        private const string CS = "4546546";
+        private static int _counter;
+        private readonly static DateTimeOffset _firstUse;
+        private int _id;
+        private int _i;
+        private string _s;
+
+        static Foo()
+        {
+            Trace.WriteLine("Startic Ctor");
+            _firstUse = DateTimeOffset.Now;
+        }
+        public Foo()
+            : this(0, string.Empty)
+        {
+            //_s = "";
+            // _s = string.Empty;
+            // _s = "A"; // override the _s data
+        }
+        public Foo(int i)
+            : this(i, string.Empty)
+        {
+        }
+        public Foo(int i, string s)
+        {
+            //_firstUse = DateTimeOffset.Now;
+            _id = _counter++;
+            _i = i;
+            _s = s;
+            string log = Format();
+            Trace.WriteLine(log);
+            LogFistUse();
+        }
+
+        private string Format()
+        {
+            _id = 10;
+            return $"{_s}: {_i}, Id = {_id}";
+        }
+        private static void LogFistUse()
+        {
+            Trace.WriteLine(_firstUse);
+        }
+    }
+}
---
+++ 002-ClassMembers/Program.cs
@@ -0,0 +1,23 @@
+﻿using _004_Class_Members;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _002_ClassMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foo f = new Foo(1, "A");
+            Thread.Sleep(2000);
+            Foo f1 = new Foo(2, "B");
+            Foo f2 = new Foo();
+
+            Console.ReadKey();
+        }
+    }
+}
