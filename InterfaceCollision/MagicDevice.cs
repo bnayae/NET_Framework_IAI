26f07c323dcdbeb5ba5161d38537acd9a4f713b8
tshaiman <tomer.shaiman@gmail.com>
2018-12-06 12:46:19 +0200

    interfaces

---
+++ InterfaceCollision/MagicDevice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceCollision
+{
+    public class MagicDevice : IPrinter, IScreen
+    {
+        public ConsoleColor Color { get; } = ConsoleColor.Gray;
+
+        public int Width { get; } = 200;
+
+        void IScreen.Print()
+        {
+            Console.WriteLine("Screen printing");
+        }
+        void IPrinter.Print()
+        {
+            Console.WriteLine("Printer printing");
+        }
+    }
+}
---
+++ InterfaceCollision/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceCollision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MagicDevice m = new MagicDevice();
+            ((IScreen)m).Print();
+            ((IPrinter)m).Print();
+
+            
+            PrintScreen(m);
+            PrintPrinter(m);
+
+        }
+
+        static void PrintPrinter(IPrinter printer)
+        {
+            Console.WriteLine("Printer....");
+            printer.Print();
+        }
+
+        static void PrintScreen(IScreen screen)
+        {
+            Console.WriteLine("Screen....");
+            screen.Print();
+        }
+    }
+}
